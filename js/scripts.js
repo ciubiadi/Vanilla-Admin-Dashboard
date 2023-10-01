82874200ceb5419022b515db3c629108345d143c
Adrian Albert Ciubucciu <aciubucciu@deloittece.com>
2023-10-01 11:20:08 +0300

    media queries for responsiveness + small js update

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,13 @@
 let sidebarOpen = false;
 let sidebar = document.getElementById('sidebar');
 
-function openSidebar() {
-    if(!sidebarOpen){
-        sidebar.classList.add('sidebar-responsive');
-        sidebarOpen = true;
-    }
-}
-
-function closeSidebar() {
+function toggleSidebar() {
     if(sidebarOpen){
         sidebar.classList.remove('sidebar-responsive');
-        sidebarOpen = false;
+    } else {
+        sidebar.classList.add('sidebar-responsive');
     }
+    sidebarOpen = !sidebarOpen;
 }
 
 /* ----------- CHARTS ----------- */
